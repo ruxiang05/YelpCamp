7f15a2b85eabd1aefb9825317600c76a554f4d11
Ruxandra Anghel <ruxandra-antonia.anghel@kcl.ac.uk>
2017-03-06 09:47:46 +0000

    Refactor project

--- app.js
+++ app.js
@@ -1,57 +1,49 @@
-var express = require("express"),
-              app = express(),
-              bodyParser = require("body-parser"),
-              mongoose = require("mongoose"),
-              passport = require("passport"),
-              LocalStrategy = require("passport-local"),
-              methodOverride = require('method-override'),
-              User = require("./models/user"),
-              Campground = require("./models/campground"),
-              Comment = require("./models/comment"),
-              seedDB = require("./seeds"),
-              port = 3000 || process.env.PORT;
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
 
-//Requiring routes
-var commentRoutes = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campground"),
-    indexRoutes = require("./routes/index");
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
 
-
-
-
-mongoose.connect("mongodb://localhost/yelp_camp");
-app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost/yelp_camp_v10");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(methodOverride('_method'));
-seedDB();
+app.use(methodOverride("_method"));
+// seedDB(); //seed the database
 
-//Passport setup
+// PASSPORT CONFIGURATION
 app.use(require("express-session")({
-    secret:"YelpCamp secret",
+    secret: "Once again Rusty wins cutest dog!",
     resave: false,
     saveUninitialized: false
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(function(req,res,next){
-    res.locals.currentUser = req.user;
-    next();
-});
-
-app.use('/', indexRoutes);
-app.use('/campgrounds', campgroundRoutes);
-app.use('/camgrounds/:id/comments', commentRoutes);
-
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-
-//Indicates that server has started
-app.listen(port, function(){
-    console.log("YelpCamp has started!");
+app.listen(3000, function(){
+   console.log("The YelpCamp Server Has Started!");
 });
--- routes/index.js
+++ routes/index.js
@@ -1,51 +1,49 @@
-var express = require("express"),
-    router = express.Router(),
-    passport = require("passport"),
-    User = require("../models/user");
+var express = require("express");
+var router  = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
 
-
-//Route route
-router.get('/', (req,res) => {
-  res.render('landing');
+//root route
+router.get("/", function(req, res){
+    res.render("landing");
 });
 
-//Register form route
-router.get("/register", function(req, res) {
-   res.render('register');
+// show register form
+router.get("/register", function(req, res){
+   res.render("register");
 });
 
-//handling register route
-router.post("/register", function(req,res){
-    var newUser = new User({username:req.body.username});
-   User.register(newUser, req.body.password, function(err,user){
-       if(err){
-           console.log(err);
-           return res.render('register');
-       }
-
-       passport.authenticate('local')(req,res,function(){
-           res.redirect('/campgrounds');
-       });
-   });
+//handle sign up logic
+router.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/campgrounds");
+        });
+    });
 });
 
-//Login route
-router.get('/login', function(req, res) {
-    res.render('login');
+//show login form
+router.get("/login", function(req, res){
+   res.render("login");
 });
 
-//handle login
-router.post('/login',passport.authenticate('local',
+//handling login logic
+router.post("/login", passport.authenticate("local",
     {
-        successRedirect: '/campgrounds',
-        failureRedirect: 'login'
-        }), function(req, res) {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
 });
 
-//logout route
-router.get('/logout', function(req, res) {
+// logout route
+router.get("/logout", function(req, res){
    req.logout();
-   res.redirect('/campgrounds');
+   res.redirect("/campgrounds");
 });
 
 //middleware
@@ -53,8 +51,7 @@ function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
-
-    res.redirect('/login');
+    res.redirect("/login");
 }
 
 module.exports = router;
