2fe96c1d275313028664804279240bd18dd88237
Ruxandra Anghel <ruxandra-antonia.anghel@kcl.ac.uk>
2017-03-06 11:47:52 +0000

    Add dynamic price and refactor landing page

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
--- models/campground.js
+++ models/campground.js
@@ -17,7 +17,8 @@ var campgroundSchema = new mongoose.Schema({
          type: mongoose.Schema.Types.ObjectId,
          ref:"Comment"
       }
-   ]
+   ],
+   price:String
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
