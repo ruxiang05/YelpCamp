d872263696fc2b613179fb783b8002c5ffe285ea
Ruxandra Anghel <ruxandra-antonia.anghel@kcl.ac.uk>
2017-03-09 18:49:26 +0000

    Update DB URL

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-
-mongoose.connect("mongodb://ruxi:ruxi@ds123930.mlab.com:23930/yelpcamp");
+//mongodb://ruxi:ruxi@ds123930.mlab.com:23930/yelpcamp"
+console.log(process.env.DATABASEURL)
+mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
